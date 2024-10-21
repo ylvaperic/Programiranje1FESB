54dc9bcb4fc8f75ae48e1fc14375fbe304effc4c
Ilva Perić <iperic03@fesb.hr>
2024-10-21 12:38:53 +0200

    Add files via upload

---
+++ Vjezba3Zadatak4.vb
@@ -0,0 +1,30 @@
+﻿'Učitavati cijele brojeve s konzole sve dok se upisuju pozitivni brojevi. Ispisati koliko je
+'korisnik upisao prostih brojeva.
+
+Imports System
+Imports System.Linq.Expressions
+
+Module Program
+    Sub Main(args As String())
+        Dim broj As Integer
+        Dim brojac As Integer = 0
+        Dim prost As Boolean
+        Console.WriteLine("Unosi pozitivne brojeve, za kraj unesi negativni.")
+        broj = Console.ReadLine()
+        Do While broj > 0
+            For i = 2 To broj - 1
+                prost = True
+                If broj Mod i = 0 Then
+                    prost = False
+                    Exit For
+                End If
+            Next
+            If prost Then
+                brojac += 1
+            End If
+            broj = Console.ReadLine()
+        Loop
+        Console.WriteLine("Uneseno je {0} prostih brojeva", brojac)
+
+    End Sub
+End Module
